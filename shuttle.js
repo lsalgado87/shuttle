573c3aabc1c2136469b96ef774357bc3d90ae0a5
lsalgado87 <salgadorluis@gmail.com>
2020-03-03 20:04:09 -0500

    shuttle assignment

---
+++ shuttle.js
@@ -0,0 +1,33 @@
+let shuttleName = "Determination"
+let speed = 17500
+let mars = 225000000
+let moon = 384400
+let miles = 0.621
+
+console.log(typeof shuttleName)
+console.log(typeof speed)
+console.log(typeof mars)
+console.log(typeof moon)
+console.log(typeof miles)
+
+let milesToMars = (mars * miles)
+
+let hoursToMars = (milesToMars / miles)
+
+let daysToMars = (hoursToMars / 24)
+
+console.log(milesToMars)
+console.log(hoursToMars)
+console.log(daysToMars)
+
+console.log(hoursToMars)
+
+console.log(shuttleName + " will take " + daysToMars + " days to reach Mars.")
+
+let milesToMoon = (moon * miles)
+
+let hoursToMoon = (milesToMoon / miles)
+
+let daysToMoon = (hoursToMoon / 24)
+
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon.")
